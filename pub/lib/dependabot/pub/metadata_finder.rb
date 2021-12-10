0a658d40782810c56fa0aadf47d59623989f18b6
Sigurd Meldgaard <sigurdm@google.com>
2021-12-10 10:24:14 +0000

    Bare bones metadata_finder

--- pub/lib/dependabot/pub/metadata_finder.rb
+++ pub/lib/dependabot/pub/metadata_finder.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+require "excon"
 require "dependabot/metadata_finders"
 require "dependabot/metadata_finders/base"
 require "dependabot/shared_helpers"
@@ -10,8 +11,22 @@ module Dependabot
       private
 
       def look_up_source
-        # TODO: Find a link to changelog on pub.dev when possible.
-        nil
+        repo = pub_listing.dig("latest", "pubspec", "repository")
+        return nil unless repo
+        
+        Source.from_url(repo)
+      end
+
+      def pub_listing
+        return @pub_listing unless @pub_listing.nil?
+
+        response = Excon.get(
+          "https://pub.dev/api/packages/#{dependency.name}",
+          idempotent: true,
+          **SharedHelpers.excon_defaults
+        )
+
+        @pub_listing = JSON.parse(response.body)
       end
     end
   end
